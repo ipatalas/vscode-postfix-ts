0fd4f52a1c4faf2ad3c78fc4a17a7ae46d698aa4
Ireneusz Patalas <fido@list.pl>
2017-04-01 13:47:40 +0200

    Extended document selector to support Javascript files as well

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,11 @@
 import * as vsc from 'vscode'
 import { PostfixCompletionProvider } from './postfixCompletionProvider'
 
-const TS_DOCUMENT_SELECTOR: vsc.DocumentSelector = 'typescript'
+const DOCUMENT_SELECTOR: vsc.DocumentSelector = ['typescript', 'javascript']
 
 export function activate (context: vsc.ExtensionContext) {
 	const provider = new PostfixCompletionProvider()
-	context.subscriptions.push(vsc.languages.registerCompletionItemProvider(TS_DOCUMENT_SELECTOR, provider, '.'))
+	context.subscriptions.push(vsc.languages.registerCompletionItemProvider(DOCUMENT_SELECTOR, provider, '.'))
 }
 
 // tslint:disable-next-line:no-empty
