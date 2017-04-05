8fddb2cbf251d4fd1e2a4cb6f378fd08ea70eb3b
Ireneusz Patalas <fido@list.pl>
2017-04-05 23:07:53 +0200

    Implemented silly but effective workaround for conflicting snippets in tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -19,8 +19,7 @@ suite('Simple template tests', () => {
 	test('var template', testTemplate('a.b', 'var', 'var name = a.b'))
 	test('const template', testTemplate('a.b', 'const', 'const name = a.b'))
 
-	// Conflicts with built-in `log` snippet - waiting for workaround
-	// test('log template', testTemplate('expr', 'log', 'console.log(expr)'))
+	test('log template', testTemplate('expr', 'log', 'console.log(expr)', false, 1))
 	test('warn template', testTemplate('expr', 'warn', 'console.warn(expr)'))
 	test('error template', testTemplate('expr', 'error', 'console.error(expr)'))
 
@@ -31,9 +30,9 @@ suite('Simple template tests', () => {
 	test('not template - inside an if', testTemplate('if (x * 100{cursor})', 'not', 'if(!(x*100))', true))
 	test('not template - inside an if with complex conditions', testTemplate('if (a > b && x * 100{cursor})', 'not', 'if(a>b&&!(x*100))', true))
 
-	// Conflicts with built-in `if` snippet
-	// test('if template', testTemplate('expr', 'if', 'if (expr) {}'))
+	test('if template', testTemplate('expr', 'if', 'if(expr){}', true, 2))
 	test('else template', testTemplate('expr', 'else', 'if(!expr){}', true))
+	test('else template - binary expression', testTemplate('x * 100', 'else', 'if(!(x*100)){}', true, 1))
 
 	test('null template', testTemplate('expr', 'null', 'if(expr===null){}', true))
 	test('notnull template', testTemplate('expr', 'notnull', 'if(expr!==null){}', true))
@@ -43,7 +42,7 @@ suite('Simple template tests', () => {
 	test('forof template', testTemplate('expr', 'forof', 'for(letitemofexpr){}', true))
 })
 
-function testTemplate (initialText: string, template: string, expectedResult: string, trimWhitespaces?: boolean) {
+function testTemplate (initialText: string, template: string, expectedResult: string, trimWhitespaces?: boolean, skipSuggestions: number = 0) {
 	return (done: MochaDone) => {
 		vsc.workspace.openTextDocument({ language: 'typescript' }).then((doc) => {
 			return vsc.window.showTextDocument(doc, vsc.ViewColumn.One).then((editor) => {
@@ -57,14 +56,18 @@ function testTemplate (initialText: string, template: string, expectedResult: st
 
 				return editor.edit(edit => {
 					edit.insert(new vsc.Position(0, 0), initialText)
-				}).then(() => {
+				}).then(async () => {
 					let pos = new vsc.Position(0, cursorIdx + template.length + 1)
 					editor.selection = new vsc.Selection(pos, pos)
 
-					return vsc.commands.executeCommand('editor.action.triggerSuggest').then(async value => {
-						await delay(getCurrentDelay())
-						return vsc.commands.executeCommand('acceptSelectedSuggestion')
-					})
+					await vsc.commands.executeCommand('editor.action.triggerSuggest')
+					await delay(getCurrentDelay())
+
+					for (let i = 0; i < skipSuggestions; i++) {
+						await vsc.commands.executeCommand('selectNextSuggestion')
+					}
+
+					return vsc.commands.executeCommand('acceptSelectedSuggestion')
 				})
 			}).then(() => {
 				let result = doc.getText()
@@ -91,10 +94,6 @@ function delay (timeout) {
 
 // for some reason editor.action.triggerSuggest needs more delay at the beginning when the process is not yet warmed up
 // let's start from high delays and then slowly go to lower delays
-let delaySteps = [3500, 2000, 1200, 700, 400, 200, 100, 50]
+let delaySteps = [3500, 2000, 1200, 700, 400, 200, 100]
 
-function getCurrentDelay () {
-	let currentDelay = (delaySteps.length > 1) ? delaySteps.shift() : delaySteps[0]
-
-	return currentDelay
-}
+const getCurrentDelay = () => (delaySteps.length > 1) ? delaySteps.shift() : delaySteps[0]
