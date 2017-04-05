08fe3fa3728371e26eb5fd5274aa5339f641c96e
Ireneusz Patalas <fido@list.pl>
2017-04-05 20:04:12 +0200

    Added some basic tests for (almost) all templates

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -10,27 +10,80 @@ import * as assert from 'assert'
 import * as vsc from 'vscode'
 import { VarTemplate } from '../src/templates/varTemplates'
 
-suite('Templates Tests', () => {
-	test('var template', (done) => {
-		vsc.workspace.openTextDocument({ language: 'typescript' }).then((doc) => {
-			let template = new VarTemplate('var')
-			let item = template.buildCompletionItem('a * 3.', new vsc.Position(0, 6))
+suite('Simple template tests', () => {
+	test('let template - binary expression', testTemplate('a * 3', 'let', 'let name = a * 3'))
+	test('let template - method call', testTemplate('obj.call()', 'let', 'let name = obj.call()'))
+	test('let template - property access expression', testTemplate('obj.a.b', 'let', 'let name = obj.a.b'))
 
-			return vsc.window.showTextDocument(doc).then(() => {
-				return vsc.window.activeTextEditor.edit(edit => {
-					edit.insert(new vsc.Position(0, 0), 'a * 3.')
+	test('var template', testTemplate('a.b', 'var', 'var name = a.b'))
+	test('const template', testTemplate('a.b', 'const', 'const name = a.b'))
+
+	// Conflicts with built-in `log` snippet - waiting for workaround
+	// test('log template', testTemplate('expr', 'log', 'console.log(expr)'))
+	test('warn template', testTemplate('expr', 'warn', 'console.warn(expr)'))
+	test('error template', testTemplate('expr', 'error', 'console.error(expr)'))
+
+	test('return template', testTemplate('expr', 'return', 'return expr'))
+
+	test('not template', testTemplate('expr', 'not', '!expr'))
+	test('not template - binary expression', testTemplate('x * 100', 'not', '!(x * 100)'))
+	test('not template - inside an if', testTemplate('if (x * 100{cursor})', 'not', 'if(!(x*100))', true))
+	test('not template - inside an if with complex conditions', testTemplate('if (a > b && x * 100{cursor})', 'not', 'if(a>b&&!(x*100))', true))
+
+	// Conflicts with built-in `if` snippet
+	// test('if template', testTemplate('expr', 'if', 'if (expr) {}'))
+	test('else template', testTemplate('expr', 'else', 'if(!expr){}', true))
+
+	test('null template', testTemplate('expr', 'null', 'if(expr===null){}', true))
+	test('notnull template', testTemplate('expr', 'notnull', 'if(expr!==null){}', true))
+	test('undefined template', testTemplate('expr', 'undefined', 'if(expr===undefined){}', true))
+	test('notundefined template', testTemplate('expr', 'notundefined', 'if(expr!==undefined){}', true))
+
+	test('forof template', testTemplate('expr', 'forof', 'for(letitemofexpr){}', true))
+})
+
+function testTemplate (initialText: string, template: string, expectedResult: string, trimWhitespaces?: boolean) {
+	return (done: MochaDone) => {
+		vsc.workspace.openTextDocument({ language: 'typescript' }).then((doc) => {
+			return vsc.window.showTextDocument(doc, vsc.ViewColumn.One).then((editor) => {
+				let cursorIdx = initialText.indexOf('{cursor}')
+				if (cursorIdx > -1) {
+					initialText = initialText.replace('{cursor}', `.${template}`)
+				} else {
+					initialText += `.${template}`
+					cursorIdx = initialText.length
+				}
+
+				return editor.edit(edit => {
+					edit.insert(new vsc.Position(0, 0), initialText)
+				}).then(() => {
+					let pos = new vsc.Position(0, cursorIdx + template.length + 1)
+					editor.selection = new vsc.Selection(pos, pos)
+
+					return vsc.commands.executeCommand('editor.action.triggerSuggest').then(async value => {
+						await delay(400)
+						return vsc.commands.executeCommand('acceptSelectedSuggestion')
+					})
 				})
 			}).then(() => {
-				return vsc.window.activeTextEditor.insertSnippet(item.insertText as vsc.SnippetString, item.range)
-			}).then(() => {
 				let result = doc.getText()
-				try {
-					assert.equal(result, 'var name = a * 3')
-					done()
-				} catch (err) {
-					done(err)
+
+				if (trimWhitespaces) {
+					result = result.replace(/\s/g, '')
 				}
+
+				assert.equal(result, expectedResult)
+
+				done()
+			}).then(undefined, (reason) => {
+				done(reason)
 			})
 		})
+	}
+}
+
+function delay (timeout) {
+	return new Promise<void>(resolve => {
+		setTimeout(resolve, timeout)
 	})
-})
+}
