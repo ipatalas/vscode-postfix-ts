568eb3d8897f9c8c44c226c23269e5c4dc62f7ab
Ireneusz Patalas <fido@list.pl>
2017-04-09 21:33:37 +0200

    Extended tests for not template (new features)

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -28,7 +28,10 @@ suite('Simple template tests', () => {
 	test('not template', testTemplate('expr', 'not', '!expr'))
 	test('not template - binary expression', testTemplate('x * 100', 'not', '!(x * 100)'))
 	test('not template - inside an if', testTemplate('if (x * 100{cursor})', 'not', 'if(!(x*100))', true))
-	test('not template - inside an if with complex conditions', testTemplate('if (a > b && x * 100{cursor})', 'not', 'if(a>b&&!(x*100))', true))
+	test('not template - inside an if with complex conditions - first expression', testComplexNotTemplate('if (a > b && x * 100{cursor})', 'not', 'if(a>b&&!(x*100))', true, 0))
+	test('not template - inside an if with complex conditions - second expression', testComplexNotTemplate('if (a > b && x * 100{cursor})', 'not', 'if(!(a>b&&x*100))', true, 1))
+	test('not template - already negated expression', testTemplate('!expr', 'not', 'expr'))
+	test('not template - already negated expression - method call', testTemplate('!x.method()', 'not', 'x.method()'))
 
 	test('if template', testTemplate('expr', 'if', 'if(expr){}', true, 2))
 	test('else template', testTemplate('expr', 'else', 'if(!expr){}', true))
@@ -45,45 +48,76 @@ suite('Simple template tests', () => {
 function testTemplate (initialText: string, template: string, expectedResult: string, trimWhitespaces?: boolean, skipSuggestions: number = 0) {
 	return (done: MochaDone) => {
 		vsc.workspace.openTextDocument({ language: 'typescript' }).then((doc) => {
-			return vsc.window.showTextDocument(doc, vsc.ViewColumn.One).then((editor) => {
-				let cursorIdx = initialText.indexOf('{cursor}')
-				if (cursorIdx > -1) {
-					initialText = initialText.replace('{cursor}', `.${template}`)
-				} else {
-					initialText += `.${template}`
-					cursorIdx = initialText.length
-				}
-
-				return editor.edit(edit => {
-					edit.insert(new vsc.Position(0, 0), initialText)
-				}).then(async () => {
-					let pos = new vsc.Position(0, cursorIdx + template.length + 1)
-					editor.selection = new vsc.Selection(pos, pos)
-
-					await vsc.commands.executeCommand('editor.action.triggerSuggest')
-					await delay(getCurrentDelay())
-
-					for (let i = 0; i < skipSuggestions; i++) {
-						await vsc.commands.executeCommand('selectNextSuggestion')
-					}
-
-					return vsc.commands.executeCommand('acceptSelectedSuggestion')
-				})
-			}).then(() => {
-				let result = doc.getText()
-
-				if (trimWhitespaces) {
-					result = result.replace(/\s/g, '')
-				}
-
-				assert.equal(result, expectedResult)
-
+			return selectAndAcceptSuggestion(
+				doc, initialText, template, skipSuggestions
+			).then(() => {
+				assertText(doc, expectedResult, trimWhitespaces)
 				done()
 			}).then(undefined, (reason) => {
 				done(reason)
 			})
 		})
 	}
+}
+
+function testComplexNotTemplate (initialText: string, template: string, expectedResult: string, trimWhitespaces?: boolean, skipSuggestions: number = 0) {
+	return (done: MochaDone) => {
+		vsc.workspace.openTextDocument({ language: 'typescript' }).then((doc) => {
+			return selectAndAcceptSuggestion(
+				doc, initialText, template
+			).then(async () => {
+				for (let i = 0; i < skipSuggestions; i++) {
+					await vsc.commands.executeCommand('workbench.action.quickOpenSelectNext')
+				}
+
+				await vsc.commands.executeCommand('workbench.action.acceptSelectedQuickOpenItem')
+				await delay(10)
+
+				assertText(doc, expectedResult, trimWhitespaces)
+				done()
+			}).then(undefined, (reason) => {
+				done(reason)
+			})
+		})
+	}
+}
+
+function selectAndAcceptSuggestion (doc: vsc.TextDocument, initialText: string, template: string, skipSuggestions: number = 0) {
+	return vsc.window.showTextDocument(doc, vsc.ViewColumn.One).then((editor) => {
+		let cursorIdx = initialText.indexOf('{cursor}')
+		if (cursorIdx > -1) {
+			initialText = initialText.replace('{cursor}', `.${template}`)
+		} else {
+			initialText += `.${template}`
+			cursorIdx = initialText.length
+		}
+
+		return editor.edit(edit => {
+			edit.insert(new vsc.Position(0, 0), initialText)
+		}).then(async () => {
+			let pos = new vsc.Position(0, cursorIdx + template.length + 1)
+			editor.selection = new vsc.Selection(pos, pos)
+
+			await vsc.commands.executeCommand('editor.action.triggerSuggest')
+			await delay(getCurrentDelay())
+
+			for (let i = 0; i < skipSuggestions; i++) {
+				await vsc.commands.executeCommand('selectNextSuggestion')
+			}
+
+			return vsc.commands.executeCommand('acceptSelectedSuggestion')
+		})
+	})
+}
+
+function assertText (doc: vsc.TextDocument, expectedResult: string, trimWhitespaces: boolean) {
+	let result = doc.getText()
+
+	if (trimWhitespaces) {
+		result = result.replace(/\s/g, '')
+	}
+
+	assert.equal(result, expectedResult)
 }
 
 function delay (timeout) {
