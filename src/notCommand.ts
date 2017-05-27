a4fd863374b41707424397b8bf471d00606a3a48
Ireneusz Patalas <fido@list.pl>
2017-05-27 23:34:28 +0200

    Changed translation for .not command

--- src/notCommand.ts
+++ src/notCommand.ts
@@ -5,23 +5,23 @@ import { invertExpression } from './utils'
 export const NOT_COMMAND = 'complete.notTemplate'
 
 export function notCommand (editor: vsc.TextEditor, position: vsc.Position, suffix: string, expressions: ts.BinaryExpression[]) {
-	vsc.window.showQuickPick(expressions.map(node => ({
-		label: node.getText(),
-		description: '',
-		detail: 'Negate this expression',
-		node: node
-	})))
-		.then(value => {
-			if (!value) {
+  vsc.window.showQuickPick(expressions.map(node => ({
+    label: node.getText(),
+    description: '',
+    detail: 'Invert this expression',
+    node: node
+  })))
+    .then(value => {
+      if (!value) {
         return undefined
-			}
+      }
 
-			editor.edit(e => {
-				const expressionBody = value.node.getText()
-				const startPos = new vsc.Position(position.line, position.character - expressionBody.length - suffix.length)
-				const range = new vsc.Range(startPos, new vsc.Position(position.line, position.character - suffix.length + 1))
+      editor.edit(e => {
+        const expressionBody = value.node.getText()
+        const startPos = new vsc.Position(position.line, position.character - expressionBody.length - suffix.length)
+        const range = new vsc.Range(startPos, new vsc.Position(position.line, position.character - suffix.length + 1))
 
-				e.replace(range, invertExpression(value.node))
-			})
-		})
+        e.replace(range, invertExpression(value.node))
+      })
+    })
 }
