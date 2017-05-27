9cc65a46c1b69aef13831a9d5ebfe5889f42dee0
Ireneusz Patalas <fido@list.pl>
2017-05-27 23:25:14 +0200

    Fixed issue #9 - snippets always on top of suggestions

--- src/completionItemBuilder.ts
+++ src/completionItemBuilder.ts
@@ -3,48 +3,50 @@ import * as vsc from 'vscode'
 const COMPLETION_ITEM_TITLE = 'Postfix templates'
 
 export class CompletionItemBuilder {
-	private item: vsc.CompletionItem
+  private item: vsc.CompletionItem
 
-	constructor (private keyword: string, private code: string) {
-		this.item = new vsc.CompletionItem(keyword, vsc.CompletionItemKind.Snippet)
-		this.item.detail = COMPLETION_ITEM_TITLE
-	}
+  constructor (private keyword: string, private code: string) {
+    this.item = new vsc.CompletionItem(keyword, vsc.CompletionItemKind.Snippet)
+    this.item.detail = COMPLETION_ITEM_TITLE
+  }
 
-	public static create = (keyword: string, code: string) => new CompletionItemBuilder(keyword, code)
+  public static create = (keyword: string, code: string) => new CompletionItemBuilder(keyword, code)
 
-	public command = (command: vsc.Command) => {
-		this.item.command = command
-		return this
-	}
+  public command = (command: vsc.Command) => {
+    this.item.command = command
+    return this
+  }
 
-	public insertText = (insertText?: string) => {
-		this.item.insertText = insertText
-		return this
-	}
+  public insertText = (insertText?: string) => {
+    this.item.insertText = insertText
+    return this
+  }
 
-	public replace = (replacement: string, position: vsc.Position, useSnippets?: boolean): CompletionItemBuilder => {
-		const codeBeforeTheDot = this.code.substr(0, this.code.lastIndexOf('.'))
+  public replace = (replacement: string, position: vsc.Position, useSnippets?: boolean): CompletionItemBuilder => {
+    const dotIdx = this.code.lastIndexOf('.')
+    const codeBeforeTheDot = this.code.substr(0, dotIdx)
 
-		if (useSnippets) {
-			const escapedCode = codeBeforeTheDot.replace('$', '\\$')
+    if (useSnippets) {
+      const escapedCode = codeBeforeTheDot.replace('$', '\\$')
 
-			this.item.insertText = new vsc.SnippetString(replacement.replace('{{expr}}', escapedCode))
-		} else {
-			this.item.insertText = replacement.replace('{{expr}}', codeBeforeTheDot)
-		}
-		// since this item needs to replace entire line it's range needs to be extended backwards to the beginning of the line/expression
-		this.item.range = new vsc.Range(position.translate(0, -this.code.length), position)
-		// setting custom `range` also requires a change on `filterText` because the text is now matched on entire range
-		this.item.filterText = codeBeforeTheDot + '.' + this.keyword
+      this.item.insertText = new vsc.SnippetString(replacement.replace('{{expr}}', escapedCode))
+    } else {
+      this.item.insertText = replacement.replace('{{expr}}', codeBeforeTheDot)
+    }
 
-		return this
-	}
+    const dotPosition = position.translate(0, -(this.code.length - dotIdx - 1))
+    this.item.additionalTextEdits = [
+      vsc.TextEdit.delete(new vsc.Range(position.translate(0, -this.code.length), dotPosition))
+    ]
 
-	public description = (description: string): CompletionItemBuilder => {
-		this.item.documentation = description
+    return this
+  }
 
-		return this
-	}
+  public description = (description: string): CompletionItemBuilder => {
+    this.item.documentation = description
 
-	public build = () => this.item
+    return this
+  }
+
+  public build = () => this.item
 }
