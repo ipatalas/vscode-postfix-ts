018071d52b5d6a92457ff9c7442c79ffe9038d4d
Ireneusz Patalas <fido@list.pl>
2019-09-22 22:25:14 +0200

    Code cleanup

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -3,12 +3,9 @@
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
 
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert'
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vsc from 'vscode'
-import { VarTemplate } from '../src/templates/varTemplates'
+
 import { getCurrentSuggestion } from '../src/postfixCompletionProvider'
 import { getCurrentDelay, delay } from './utils'
 
