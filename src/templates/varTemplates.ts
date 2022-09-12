649f243d3f7bd61ab86d20724805f50d7c88f624
Ireneusz Patalas <fido@list.pl>
2022-09-12 17:45:27 +0200

    Handle `new` expressions

--- src/templates/varTemplates.ts
+++ src/templates/varTemplates.ts
@@ -1,6 +1,8 @@
 import * as ts from 'typescript'
 import { CompletionItemBuilder } from '../completionItemBuilder'
 import { BaseExpressionTemplate } from './baseTemplates'
+import * as vsc from 'vscode'
+import _ = require('lodash')
 
 export class VarTemplate extends BaseExpressionTemplate {
   constructor(private keyword: 'var' | 'let' | 'const') {
@@ -8,9 +10,12 @@ export class VarTemplate extends BaseExpressionTemplate {
   }
 
   buildCompletionItem(node: ts.Node, indentSize?: number) {
+    const inferVarNameEnabled = vsc.workspace.getConfiguration('postfix', null).get<boolean>('inferVariableName')
+    const suggestedVarName = inferVarNameEnabled ? this.inferVarName(node) : undefined;
+
     return CompletionItemBuilder
       .create(this.keyword, node, indentSize)
-      .replace(this.keyword + ' ${1:name} = {{expr}}$0', true)
+      .replace(`${this.keyword} \${1:${suggestedVarName ?? 'name'}} = {{expr}}$0`, true)
       .build()
   }
 
@@ -20,5 +25,17 @@ export class VarTemplate extends BaseExpressionTemplate {
       && !this.inFunctionArgument(node)
       && !this.inVariableDeclaration(node)
       && !this.inAssignmentStatement(node)
+  }
+
+  private inferVarName(node: ts.Node) {
+    if (ts.isNewExpression(node)) {
+      const buildVarName = (name: string) => name && _.lowerFirst(name)
+      if (ts.isIdentifier(node.expression)) {
+        return buildVarName(node.expression.text)
+      } else if (ts.isPropertyAccessExpression(node.expression)) {
+        return buildVarName(node.expression.name.text)
+      }
+    }
+
   }
 }
