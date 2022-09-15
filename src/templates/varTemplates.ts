11c40dd0a36c65b07d57b573f9963f6ad455a9b9
Vitaly Turovsky <vital2580@icloud.com>
2022-09-15 04:57:42 +0300

    enable proposal of multiple variants

--- src/templates/varTemplates.ts
+++ src/templates/varTemplates.ts
@@ -5,7 +5,8 @@ import { BaseExpressionTemplate } from './baseTemplates'
 import { getConfigValue } from '../utils'
 
 export class VarTemplate extends BaseExpressionTemplate {
-  private static MethodCallRegex = /^(get|read|create|retrieve|select|modify|update|use|find)(?<name>[A-Z].+?)(By.*)?$/
+  private static MethodCallRegex = /^(get|read|create|retrieve|select|modify|update|use|find)(?<name>[A-Z].+?)?$/
+  private static CleanNameRegex = /((By|With|From).*$)|(Sync$)/
 
   constructor(private keyword: 'var' | 'let' | 'const') {
     super(keyword)
@@ -13,11 +14,12 @@ export class VarTemplate extends BaseExpressionTemplate {
 
   buildCompletionItem(node: ts.Node, indentSize?: number) {
     const inferVarNameEnabled = getConfigValue<boolean>('inferVariableName')
-    const suggestedVarName = inferVarNameEnabled ? this.inferVarName(node) : undefined;
+    const suggestedVarName = inferVarNameEnabled ? this.inferVarName(node) : undefined
+    const nameSnippet = suggestedVarName?.length > 1 ? `\${1|${suggestedVarName.join(',')}|}` : `\${1:${suggestedVarName?.[0] ?? 'name'}}`
 
     return CompletionItemBuilder
       .create(this.keyword, node, indentSize)
-      .replace(`${this.keyword} \${1:${suggestedVarName ?? 'name'}} = {{expr}}$0`, true)
+      .replace(`${this.keyword} ${nameSnippet} = {{expr}}$0`, true)
       .build()
   }
 
@@ -33,12 +35,16 @@ export class VarTemplate extends BaseExpressionTemplate {
     const buildVarName = (name: string) => name && _.lowerFirst(name)
 
     if (ts.isNewExpression(node)) {
-      return buildVarName(this.inferNewExpressionVar(node))
+      return [buildVarName(this.inferNewExpressionVar(node))]
     } else if (ts.isCallExpression(node)) {
       const methodName = this.getMethodName(node)
-      const matches = VarTemplate.MethodCallRegex.exec(methodName)
+      const name = VarTemplate.MethodCallRegex.exec(methodName)?.groups?.name
+      const cleanerVariant = name?.replace(VarTemplate.CleanNameRegex, '')
+      if (!name) {
+        return
+      }
 
-      return buildVarName(matches?.groups?.name)
+      return [...cleanerVariant && cleanerVariant !== name ? [cleanerVariant] : [], name].map(buildVarName)
     }
   }
 
