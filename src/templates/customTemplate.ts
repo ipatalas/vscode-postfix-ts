1cfbd092eb1f7d3eeaebeea27cd68566d5366432
Ireneusz Patalas <fido@list.pl>
2022-09-16 15:07:57 +0200

    Code cleanup

--- src/templates/customTemplate.ts
+++ src/templates/customTemplate.ts
@@ -4,14 +4,14 @@ import { CompletionItemBuilder } from '../completionItemBuilder'
 
 export class CustomTemplate extends BaseTemplate {
   private conditionsMap = new Map<string, (node: ts.Node) => boolean>([
-    ['type', (node: ts.Node) => this.isTypeNode(node)],
-    ['identifier', (node: ts.Node) => this.isIdentifier(node)],
-    ['string-literal', (node: ts.Node) => this.isStringLiteral(node)],
-    ['expression', (node: ts.Node) => this.isExpression(node)],
-    ['binary-expression', (node: ts.Node) => this.isBinaryExpression(node)],
-    ['unary-expression', (node: ts.Node) => this.isUnaryExpression(node.parent)],
-    ['new-expression', (node: ts.Node) => this.isNewExpression(node)],
-    ['function-call', (node: ts.Node) => this.isCallExpression(node)]
+    ['type', node => this.isTypeNode(node)],
+    ['identifier', node => this.isIdentifier(node)],
+    ['string-literal', node => this.isStringLiteral(node)],
+    ['expression', node => this.isExpression(node)],
+    ['binary-expression', node => this.isBinaryExpression(node)],
+    ['unary-expression', node => this.isUnaryExpression(node.parent)],
+    ['new-expression', node => this.isNewExpression(node)],
+    ['function-call', node => this.isCallExpression(node)]
   ])
 
   constructor (name: string, private description: string, private body: string, private when: string[]) {
